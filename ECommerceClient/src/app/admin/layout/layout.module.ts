8f3992a6598a92085d4001a702f786d5777e6b9a
Dogukan Koc <dk.dogukankoc@gmail.com>
2023-07-20 17:05:35 +0300

    AddedAngularMaterialLibraryforAdminLayoutandAddedSidenavSidebarComponents

--- ECommerceClient/src/app/admin/layout/layout.module.ts
+++ ECommerceClient/src/app/admin/layout/layout.module.ts
@@ -3,10 +3,13 @@ import { CommonModule } from '@angular/common';
 import { ComponentsModule } from './components/components.module';
 import { LayoutComponent } from './layout.component';
 import { RouterModule } from '@angular/router';
+import {MatSidenavModule} from '@angular/material/sidenav';
+
+
 
 @NgModule({
   declarations: [LayoutComponent],
-  imports: [CommonModule, ComponentsModule,RouterModule],
+  imports: [CommonModule, ComponentsModule,RouterModule,MatSidenavModule],
   exports: [LayoutComponent],
 })
 export class LayoutModule {}

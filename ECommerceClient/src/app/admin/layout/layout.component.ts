4d669be54d0207225348d8bf0a4bcef0dd781c13
Dogukan Koc <dk.dogukankoc@gmail.com>
2023-07-21 13:38:15 +0300

    AddedAlertifyLibraryandCreatedAlertifyService

--- ECommerceClient/src/app/admin/layout/layout.component.ts
+++ ECommerceClient/src/app/admin/layout/layout.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { AlertifyService, MessageType, Position } from 'src/app/services/admin/alertify.service';
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.scss']
 })
-export class LayoutComponent {
-
+export class LayoutComponent implements OnInit{   
+constructor(private alertify : AlertifyService ) {}  
+ngOnInit(): void {
+  this.alertify.message('Merhaba', {
+    messageType: MessageType.Success,
+    delay: 5,
+    position: Position.TopRight,
+  });
 }
+}
+

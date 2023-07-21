3b7c7aca8fce8f872c40d603edfe6cd8e9a88240
Dogukan Koc <dk.dogukankoc@gmail.com>
2023-07-21 14:29:38 +0300

    AddedToastrLibraryandCreatedToastrService

--- ECommerceClient/src/app/app.component.ts
+++ ECommerceClient/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CustomToastrService, ToastrMessageType, ToastrPosition } from './services/ui/custom-toastr.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ECommerceClient';
-}
+  constructor(private toastrService: CustomToastrService){
+    toastrService.message('Hello', 'Doğukan', {
+      messageType: ToastrMessageType.Info,
+      position: ToastrPosition.TopCenter,
+    });
+    toastrService.message('Hello', 'Doğukan', {
+      messageType: ToastrMessageType.Error,
+      position: ToastrPosition.BottomLeft,
+    });
+    toastrService.message('Hello', 'Doğukan', {
+      messageType: ToastrMessageType.Warning,
+      position: ToastrPosition.BottomRight,
+    });
+    toastrService.message('Hello', 'Doğukan', {
+      messageType: ToastrMessageType.Success,
+      position: ToastrPosition.TopLeft,
+    });
+    toastrService.message('Hello', 'Doğukan', {
+      messageType: ToastrMessageType.Warning,
+      position: ToastrPosition.TopRight,
+    });
+  }}
